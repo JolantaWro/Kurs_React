9bc39fac30863c4f5b326416f30b1de0085ec1b0
JolantaWro <jolanta.woronowska@gmail.com>
2023-02-28 09:13:59 +0100

    up_5 i 6

--- 01_Dzien_1/03_Wyrazenia/05_Zadanie_5/js/app.js
+++ 01_Dzien_1/03_Wyrazenia/05_Zadanie_5/js/app.js
@@ -1,25 +1,23 @@
-// import React from "react";
-// import { createRoot } from "react-dom/client";
-//
-// const container = document.getElementById("app");
-// const root = createRoot(container);
-// root.render(<h1>Hello, World!</h1>);
-
 import React from "react";
 import { createRoot } from "react-dom/client";
-
+import person from "../../03_Zadanie_3/js/data/person";
 const firstNumber = prompt("Podaj pierwszą liczbe")
 const secondNumber = prompt("Podaj drugą liczbę")
 const singValue = prompt("Wprowadz znak działania")
 
+
+function calc(a, b, operation) {
+    if (operation === "+") {
+        return <h1>{parseFloat(a) + parseFloat(b)}</h1>;
+    } else if (operation === "-") {
+       return <h2>{parseFloat(a) - parseFloat(b)}</h2>;
+    } else if (operation === "*") {
+        return <h3>{parseFloat(a) * parseFloat(b)}</h3>;
+    } else if (operation === "/") {
+        return <h4>{parseFloat(a) / parseFloat(b)}</h4>;
+    }
+}
 const container = document.getElementById("app");
 const root = createRoot(container);
-if (singValue === "+") {
-    root.render(<h1>{parseFloat(firstNumber) + parseFloat(secondNumber)}</h1>);
-} else if (singValue === "-") {
-    root.render(<h2>{parseFloat(firstNumber) - parseFloat(secondNumber)}</h2>);
-} else if (singValue === "*") {
-    root.render(<h3>{parseFloat(firstNumber) * parseFloat(secondNumber)}</h3>);
-} else if (singValue === "/") {
-    root.render(<h4>{parseFloat(firstNumber) / parseFloat(secondNumber)}</h4>);
-}
+root.render(<div>{calc(firstNumber, secondNumber, singValue)}</div>);
+// calc(firstNumber, secondNumber, singValue)
