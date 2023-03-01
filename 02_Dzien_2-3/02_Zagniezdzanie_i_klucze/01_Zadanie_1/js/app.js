67f5a57c0492a5b80eb566a6d1b64b324a72cc5e
JolantaWro <jolanta.woronowska@gmail.com>
2023-03-01 14:24:03 +0100

    up day3

--- 02_Dzien_2-3/02_Zagniezdzanie_i_klucze/01_Zadanie_1/js/app.js
+++ 02_Dzien_2-3/02_Zagniezdzanie_i_klucze/01_Zadanie_1/js/app.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { createRoot } from "react-dom/client";
+import people from "./data/people";
+
+// const printPeople = people.map(element => <div>{element.name} {element.surname}</div> )
+
+const listPeople = people.map((element, index) => <li key={index}>{element.name} {element.surname}</li>);
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<h1>Hello, World!</h1>);
+// root.render(
+//     <>
+//         {printPeople}
+//     </>);
+
+root.render(
+    <div>
+        <ul>
+            {listPeople}
+        </ul>
+    </div>)
