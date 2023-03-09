e76d6c7d1d72324987176e378f629629f6e08ea0
JolantaWro <jolanta.woronowska@gmail.com>
2023-03-09 13:17:53 +0100

    up

--- 06_Dzien_9-10/03_Warunkowe_blokowanie_wyswietlania/01_Zadanie_1/js/app.js
+++ 06_Dzien_9-10/03_Warunkowe_blokowanie_wyswietlania/01_Zadanie_1/js/app.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import SecretStuff from "./SecretStuff";
 
+const App = () => <SecretStuff password="kajak" correctPassword="kajak" secret="kajak" />
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(<App />);
