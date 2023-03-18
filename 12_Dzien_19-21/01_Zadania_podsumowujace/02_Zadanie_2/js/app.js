9991c5ec0fc98ca39d0380b6ca0658a2761062db
JolantaWro <jolanta.woronowska@gmail.com>
2023-03-18 13:42:04 +0100

    select and textarea

--- 12_Dzien_19-21/01_Zadania_podsumowujace/02_Zadanie_2/js/app.js
+++ 12_Dzien_19-21/01_Zadania_podsumowujace/02_Zadanie_2/js/app.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { createRoot } from "react-dom/client";
+import Notes from "./Notes";
+
+const App = () => <Notes />
 
 const container = document.getElementById("app");
 const root = createRoot(container);
-root.render(<h1>Hello, World!</h1>);
+root.render(<><App /></>);
